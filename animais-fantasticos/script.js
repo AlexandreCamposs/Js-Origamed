e01175a500576178542076c62c2c5c529b109846
AlexandreCamposs <89165912+AlexandreCamposs@users.noreply.github.com>
2022-08-19 18:44:47 -0300

    att

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -79,3 +79,112 @@ imagens3.forEach((item) => {
 const link = document.querySelector('a[href^="http"]');
 // link.setAttribute("href", "https://linkedin.com/");
 // console.log(link);
+
+// Exercício Dimensões e Distâncias.
+// Verifique a distância da primeira imagem  em relação ao topo da página
+const img = document.querySelector("img");
+const imgTop = img.offsetTop;
+// console.log(imgTop);
+
+function somaImagens() {
+  // Retorne a soma da largura de todas as imagens
+  const imgT = document.querySelectorAll("img");
+  let soma = 0;
+  imgT.forEach((imagem) => {
+    soma = soma + imagem.offsetWidth;
+  });
+  // console.log(soma);
+}
+window.onload = function () {
+  somaImagens();
+};
+// Verifique se os links da página possuem  o mínimo recomendado para telas utilizadas  com o dedo. (48px/48px de acordo com o google)
+const linkss = document.querySelectorAll("a");
+
+linkss.forEach((links) => {
+  const linkWidth = links.offsetWidth;
+  const linkHeight = links.offsetHeight;
+  if (linkWidth >= 47 && linkHeight >= 47) {
+    // console.log("Possui boa acessibilidade!");
+  } else {
+    // console.log("Não possui boa acessibilidade!");
+  }
+});
+// Se o browser for menor que 720px, adicione a classe menu-mobile ao menu
+const browerSmall = window.matchMedia("(max-width: 720px)").matches;
+
+if (browerSmall) {
+  const menu = document.querySelector(".menu");
+  menu.classList.add("menu-mobile");
+}
+
+// Exercício Event
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const linksss = document.querySelectorAll('a[href^="#"]');
+// console.log(linkss)
+
+function handleLink(event) {
+  event.preventDefault();
+  linksss.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  event.currentTarget.classList.add("ativo");
+  // console.log(event);
+}
+linksss.forEach((link) => {
+  link.addEventListener("click", handleLink);
+});
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+
+const elementos = document.querySelectorAll("body *");
+
+// function handleElement(event) {
+//   console.log(event.currentTarget);
+// }
+
+// function handleElement(event) {
+//   event.currentTarget.remove();
+// }
+// elementos.forEach((elemento) => {
+//   elemento.addEventListener("click", handleElement);
+// });
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+
+// function clickT(event) {
+//   console.log(event.key);
+//   if( event.key === 't') {
+//     document.documentElement.classList.toggle('textomaior');
+//   }
+// }
+
+// window.addEventListener("keydown", clickT);
+
+// Exercício transversing
+// Duplique o menu e adicione ele em copy
+// const menu = document.querySelector('.menu');
+// const copy = document.querySelector('.copy');
+
+// const clone = menu.cloneNode(true);
+
+// copy.appendChild(clone)
+
+// Selecione o primeiro DT da dl de Faq
+const faq = document.querySelector(".faq");
+const primeiroDt = faq.querySelector("dt");
+
+
+// Selecione o DD referente ao primeiro DT
+const proxDD = primeiroDt.nextElementSibling;
+// console.log(proxDD)
+
+
+// Substitua o conteúdo html de .faq pelo de .animais
+const animaiss = document.querySelector('.animais');
+faq.innerHTML = animaiss.innerHTML;
